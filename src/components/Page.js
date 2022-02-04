173871e0ebf441a104e00364e0d30050b60db58c
hyeon <liebe244@naver.com>
2022-02-04 22:31:10 +0900

    detail page

---
+++ src/components/Page.js
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+
+function Movie({ coverImg, title, runtime, rating, description, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>{title}</h2>
+      <h4>{runtime} minutes</h4>
+      <h4>{rating}</h4>
+      <p>{description}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  runtime: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Page from "../components/Page";
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
   const { id } = useParams();
   const getMovie = async () => {
-    const json = await fetch(
-      `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+    console.log(json);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>detail</h1>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <Page
+            coverImg={movie.medium_cover_image}
+            title={movie.title}
+            runtime={movie.runtime}
+            rating={movie.rating}
+            description={movie.description_full}
+            genres={movie.genres}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
