198f563c850de05916c0ad128f0539f71a048a33
hyeon <liebe244@naver.com>
2022-02-06 16:53:07 +0900

    movie page CSS

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,17 +4,21 @@ import styles from "../Movie.module.css";
 
 function Movie({ id, coverImg, title, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+    <div className={styles.movieFlex}>
+      <img className={styles.img} src={coverImg} alt={title} />
+      <div>
+        <h2>
+          <Link className={styles.a} to={`/movie/${id}`}>
+            {title}
+          </Link>
+        </h2>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)} ...` : summary}</p>
+        <ul>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
